c10d77c71a75b11f9537f6c4a30256402cdaec2f
Tariq <tariq.dev43@gmail.com>
2024-07-14 09:57:09 -0700

    "Updated Modal.js and index.html: modified modal menu and close button styles, added event listeners for modal open and close functionality."

--- Js/Modal.js
+++ Js/Modal.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //  List and Append Modal Menu to list
   let modalMenus = document.querySelectorAll('[data-type="modal-menu"]');
   modalMenus.forEach((menu) => {
-    menu.style.display = "none";
+    // menu.style.display = "none";
     modalMenusList[menu.dataset.id] = menu;
   });
 
@@ -18,11 +18,36 @@ document.addEventListener("DOMContentLoaded", () => {
   //  List and Append Modal Close to list
   let modalCloseBtn = document.querySelectorAll('[data-type="modal-close"]');
   modalCloseBtn.forEach((closeBtn) => {
-    closeBtn.style.display = "none";
     modalCloseBtnList[closeBtn.dataset.id] = closeBtn;
   });
 
+  // loop over buttons and adding listeners to open close
   for (let [btnId, btn] of Object.entries(modalButtonsList)) {
-    let modalMenu = modalButtonsList[btnId];
+    let modalMenu = modalMenusList[btnId];
+    let modalCloseBtn = modalCloseBtnList[btnId];
+    // if (modalCloseBtn) {
+    //   modalCloseBtn.addEventListener("click", () => {
+    //     modalMenu.style.display = "none";
+    //   });
+    // }
+
+    // let handleClickOutside = () => {
+    //   if (modalMenu.style.display === "block") {
+
+    // };
+    // window.addEventListener("click", handleClickOutside);
+
+    btn.addEventListener("click", () => {
+      if (modalMenu.style?.display === "none") {
+        modalMenu.style.display = "block";
+        modalMenu.style.position = "absolute";
+        modalMenu.style.position = "absolute";
+        modalMenu.style.top = "50%";
+        modalMenu.style.left = "50%";
+        modalMenu.style.transform = "translate(-50%, -50%)";
+      } else {
+        modalMenu.style.display = "none";
+      }
+    });
   }
 });
