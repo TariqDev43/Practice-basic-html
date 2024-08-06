850649864973c13c21ddac83eb79d45d97244e17
Tariq <tariq.dev43@gmail.com>
2024-08-06 21:28:18 +0500

    "Added data-accordion-group attribute to accordion menus and updated JavaScript to handle accordion groups"

--- Js/Accordian.js
+++ Js/Accordian.js
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
     2. data-id = It should be a unique Id
     [Menu] :-
     Create A Menu Element it can be div or anything you like.
-    It should have 2 data- attributes and Optional data-default-open.
+    It should have 2 data- attributes and 2 Optional data- attributes.
     1. data-type = It should have "accordion-menu"
     2. data-id = It should be ID of its trigger button
     3. (Optional) data-default-open = if it has true menu will be opened default
+    3. (Optional) data-accordion-group = accordion-menu with same data-accordion-group
+                  will have only 1 accordion open at a time.
     Note:
         If no data-default-open is provided it will default to
         false.
@@ -28,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
           data-id="accordion-1"
           data-default-open="true"
           data-type="accordion-menu"
+          data-accordion-group="group1"
         >
           Accordion Body
         </div>
@@ -58,11 +61,14 @@ document.addEventListener("DOMContentLoaded", () => {
     accordionMenuList[menuId] = menu;
   });
 
+  // Lopping over all accordion trigger and adding click listener to open close accordion
   for (let [btnId, btn] of Object.entries(accordionBtnList)) {
     let menu = accordionMenuList[btnId];
     if (!menu) {
       return;
     }
+
+    let menuGroupName = menu.getAttribute("data-accordion-group");
 
     // Add event listener to the button
     btn.addEventListener("click", () => {
@@ -70,9 +76,27 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.style.display = "block";
         menu.style.top = `${btn.offsetTop + btn.offsetHeight}px`;
         menu.style.left = `${btn.offsetLeft}px`;
+
+        // Getting list of all Menu Group with same accordion-group id as in current menu
+        if (menuGroupName) {
+          let menuGroup = document.querySelectorAll(
+            `[data-accordion-group="${menuGroupName}"]`
+          );
+          toggleAccordion(menuGroup, btnId);
+        }
       } else {
         menu.style.display = "none";
       }
     });
   }
+
+  // Function to hide all the other accordion who have same data-accordion-group value
+  const toggleAccordion = (group, currentAccordionId) => {
+    group.forEach((menu) => {
+      let menuId = menu.getAttribute("data-id");
+      if (menuId !== currentAccordionId) {
+        menu.style.display = "none";
+      }
+    });
+  };
 });
